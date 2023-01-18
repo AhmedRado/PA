45b1a44dab3679603ac08836eede1cf974bce134
AhmedRado <ahmed.abdelfattah@tuhh.de>
2023-01-18 18:13:34 +0100

    changes too much

--- hand_model/scripts/zero_joints.py
+++ hand_model/scripts/zero_joints.py
@@ -2,10 +2,7 @@
 import rospy
 from std_msgs.msg import Float64
 
-index_joint_1_msg = Float64()
-index_joint_2_msg = Float64()
-index_joint_3_msg = Float64()
-
+zero_joint_msg = Float64()
 
 def main():
     rospy.init_node("zero_joints")
@@ -14,11 +11,9 @@ def main():
     index_joint_3_pub = rospy.Publisher("/hand/index_joint_3_controller/command", Float64, queue_size=1)
     rate = rospy.Rate(50.0)
     while not rospy.is_shutdown():
-        index_joint_3_pub.publish(index_joint_3_msg)
-        index_joint_2_pub.publish(index_joint_2_msg)
-        index_joint_1_pub.publish(index_joint_1_msg)
-        
-        
+        index_joint_3_pub.publish(zero_joint_msg)
+        index_joint_2_pub.publish(zero_joint_msg)
+        index_joint_1_pub.publish(zero_joint_msg)
         rate.sleep()
 
 if __name__ == "__main__":
